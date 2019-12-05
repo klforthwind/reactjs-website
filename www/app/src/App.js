701cd316affd043d7e9d12958ff9e3918393410e
klforthwind <kl.forthwind@gmail.com>
2019-12-05 10:08:22 -0600

    add some contents

--- www/app/src/App.js
+++ www/app/src/App.js
@@ -5,6 +5,13 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
+        <h1>Forthwind's stuff</h1>
+			  <img src="400x400Forthwind.jpg" alt="Forthwind" height="320px" width="320px" />
+            <div id="links">
+                <a href="https://www.klforthwind.com/blog">Blog</a>
+                &bull;
+                <a href="https://github.com/klforthwind">Github</a>
+            </div>
       </div>
     );
   }
