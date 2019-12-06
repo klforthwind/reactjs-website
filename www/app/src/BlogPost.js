290450bb9d3b96baa965b17be8f9c80fceb1b034
klforthwind <kl.forthwind@gmail.com>
2019-12-06 10:48:20 -0600

    fill out BlogPost

--- www/app/src/BlogPost.js
+++ www/app/src/BlogPost.js
@@ -1,7 +1,27 @@
+const markdown = require("markdown-js");
+import BlogHome from './BlogHome';
 import React from 'react';
+import Info from './Info';
+const fs = require("fs");
 
-const BlogPost = () => {
-    return (<div></div>);
+const BlogPost = ({ match}) => {
+    let post_data = "";
+    const { params: { location } } = match;
+    let loc = "posts/" + { location } + ".md";
+    try {
+        let str = fs.readFileSync(loc, "utf8");
+        let result = markdown.makeHtml(str);
+        post_data = result;
+    } catch(error) {
+        return(<BlogHome />);
+    }
+    return (
+        <div>
+            <a href="/blog/"><ul id="clickable">{"<--Back To Blog"}</ul></a><br />
+            <div dangerouslySetInnerHTML={{__html:post_data}} />
+            <Info />
+        </div>
+    );
 }
 
 export default BlogPost;
