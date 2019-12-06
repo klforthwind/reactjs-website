295c5006e03e3d0b4466ac451a4ceb1dfdbe1ca5
klforthwind <kl.forthwind@gmail.com>
2019-12-06 08:39:10 -0600

    fix bloghome

--- www/app/src/BlogHome.js
+++ www/app/src/BlogHome.js
@@ -1,18 +1,18 @@
+import logo from './400x400Forthwind.jpg';
 const db = require('./database');
 import React from 'react';
 import Info from './Info';
-
 
 const BlogHome = () => {
     return (
         <div>
             <div id="picture">
-				<a href="/blog/"><img src="../400x400Forthwind.jpg" alt="Forthwind" height="128px" width="128px" /></a>
+				<a href="/blog/"><img src={logo} alt="Forthwind" height="128px" width="128px" /></a>
 				<h1>Forthwind's blog</h1>
 				<h2>Uhhh... I tried.</h2>
 			</div>
 			{/* Place to add blog articles below here */}
-			<div id="root">{db.getPostList()}</div>
+			<div id="root"><div dangerouslySetInnerHTML={{__html:db.getPostList()}} /></div>
             <Info />
         </div>);
 }
--- www/app/src/Info.js
+++ www/app/src/Info.js
@@ -1,10 +1,12 @@
+import gmail from './gmail-logo.png';
+import github from './github.png';
 import React from 'react';
 
 const Info = () => {
     return (
         <div id="info">
-			<p style={{ width: "100px", margin: "0 auto", background: "#333", padding: "10px 10px 0 10px" }}><a href="mailto:klforthwind@gmail.com"><img alt="Gmail" style={{width:"40px",height:"40px"}} src="./gmail-logo.png" /></a>
-			<a style={{ float:"right" }} href="https://www.github.com/klforthwind"><img  alt="Github" style={{width:'40px',height:'40px'}} src="./github.png" /></a></p>
+			<p style={{ width: "100px", margin: "0 auto", background: "#333", padding: "10px 10px 0 10px" }}><a href="mailto:klforthwind@gmail.com"><img alt="Gmail" style={{width:"40px",height:"40px"}} src={gmail} /></a>
+			<a style={{ float:"right" }} href="https://www.github.com/klforthwind"><img  alt="Github" style={{width:'40px',height:'40px'}} src={github} /></a></p>
 		</div>
     );
 }
